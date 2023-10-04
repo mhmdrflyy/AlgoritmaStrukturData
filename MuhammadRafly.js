614140a08803648d06141d82b319ec16984d245b
mhmdrflyy <114544930+mhmdrflyy@users.noreply.github.com>
2023-10-04 20:48:16 +0700

    Add files via upload

---
+++ MuhammadRafly.js
@@ -0,0 +1,6 @@
+var firstname = "Muhammad";
+var lastname = "Rafly";
+var age = 20;
+var isMarried = false;
+
+console.log("Hallo, Nama Saya "+firtsname+lastname,",umur saya baru menginjak "+age, "tahun")
